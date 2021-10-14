7cfac7f72bb8f482d451b51120262b55c0837338
Courtney Morris <shull.courtney@gmail.com>
2021-10-14 12:25:21 -0600

    carousel cards

--- src/components/styled-components/carousel.js
+++ src/components/styled-components/carousel.js
@@ -12,6 +12,44 @@ export default function HomeCarousel() {
           </Carousel.Caption>
         </div>
       </Carousel.Item>
+
+      <Carousel.Item className="carousel-item card-two">
+        <div className="inner-box">
+          <img
+            className="grid-column-one"
+            src="https://i.ibb.co/XXDm00d/scrunchie-Book.jpg"
+          />
+          <img
+            className="grid-column-two"
+            src="https://i.ibb.co/WPmQyZp/teal-Earrings.jpg"
+          />
+          <img
+            className="grid-column-three"
+            src="https://i.ibb.co/VD7QbG1/scrunchies-Book.jpg"
+          />
+          <Carousel.Caption className="caption">
+            <h2 className="image-text">ALL HANDMADE</h2>
+          </Carousel.Caption>
+        </div>
+      </Carousel.Item>
+
+      <Carousel.Item className="carousel-item card-three">
+        <div className="inner-box">
+          <Carousel.Caption className="caption">
+            <h1>EVERY PIECE</h1>
+            <h2>EVERY STITCH</h2>
+            <h3>EVERY DETAIL</h3>
+          </Carousel.Caption>
+        </div>
+      </Carousel.Item>
+
+      <Carousel.Item className="carousel-item card-four">
+        <div className="inner-box">
+          <Carousel.Caption className="caption">
+            <h1>Quality that lasts</h1>
+          </Carousel.Caption>
+        </div>
+      </Carousel.Item>
     </Carousel>
   );
 }
